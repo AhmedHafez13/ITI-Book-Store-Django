899cef218eb4d3a6adb472107eceedee6b4d14c7
Ahmed Hafez <ahmedhafez13@yahoo.com>
2022-04-21 20:35:54 +0200

    Add Edit functionality

--- books/urls.py
+++ books/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import index, create_book, show_book, delete_book, show_author
+from .views import index, create_book, show_book, edit_book, delete_book, show_author
 
 urlpatterns = [
     path('', index, name="index"),
     path('books/create/', create_book, name="create_book"),
-    path('books/delete/<int:book_id>', delete_book, name="create_book"),
     path('books/<int:book_id>/', show_book, name="book_details"),
+    path('books/edit/<int:book_id>', edit_book, name="edit_book"),
+    path('books/delete/<int:book_id>', delete_book, name="delete_book"),
     path('authors/<int:author_id>/', show_author, name="author_details")
 ]
 
--- books/views.py
+++ books/views.py
@@ -26,13 +26,28 @@ def create_book(request):
     else:
         return HttpResponse("Method not allowed")
 
-    return render(request, "books/create_book.html", context={"form": form})
+    return render(request, "books/book_form.html", context={"form": form})
 
 
 def show_book(request, book_id):
     book = Book.objects.get(id=book_id)
     book.cover = os.path.basename(book.cover_image.name)
     return render(request, "books/book_details.html", context={'book': book})
+
+
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            book = form.save()
+            return redirect("book_details", book_id=book.id)
+    elif request.method == "GET":
+        form = BookForm(instance=book)
+    else:
+        return HttpResponse("Method not allowed")
+
+    return render(request, "books/book_form.html", context={"form": form})
 
 
 def delete_book(request, book_id):
